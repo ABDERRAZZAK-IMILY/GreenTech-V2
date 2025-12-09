6c8cb80eb8b018737cfd22b02873975ccb65bc1c
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-12-09 10:19:17 +0100

    Feat : update UserRepository to use email instead of login for user retrieval

--- src/main/java/com/greentechinnovators/backend/repository/UserRepository.java
+++ src/main/java/com/greentechinnovators/backend/repository/UserRepository.java
@@ -7,8 +7,8 @@ import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User , String> {
 
-    Optional<User> findByLogin(String login);
+    Optional<User> findByEmail(String email);
 
-    boolean existsByLogin(String login);
+    boolean existsByEmail(String login);
 
 }
