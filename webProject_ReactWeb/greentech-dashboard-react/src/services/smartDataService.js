60f655334c381ae285190db6d1778190256cf059
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-12-09 11:18:34 +0100

    Feat : add SmartDataService for handling API requests and metrics calculations

---
+++ webProject_ReactWeb/greentech-dashboard-react/src/services/smartDataService.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import AuthService from './authService';
+
+const API_URL = 'http://localhost:8080/api/v1';
+
+const axiosInstance = axios.create({
+  baseURL: API_URL
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = AuthService.getToken();
+    if (token) {
+      config.headers['Authorization'] = 'Bearer ' + token;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 403) {
+      AuthService.logout();
+    }
+    return Promise.reject(error);
+  }
+);
+
+class SmartDataService {
+    
+    getMetrics(type) {
+        return axiosInstance.get(`/metrics/${type}`);
+    }
+
+    calculateTotal(dataList) {
+        if (!dataList || dataList.length === 0) return 0;
+        return dataList.reduce((sum, item) => sum + (item.value || 0), 0);
+    }
+}
+
+export default new SmartDataService();
