9e561656b520ec907366b2cf844f83327c6e9693
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-12-09 12:31:37 +0100

    made a energymonitor entity

---
+++ GreenTech-V2/src/main/java/com/greentechinnovators/backend/entity/EnergyMonitor.java
@@ -0,0 +1,28 @@
+package com.greentechinnovators.backend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document("EnergyMonitor")
+public class EnergyMonitor {
+    @Id
+    private String id;
+
+    private String location;
+    private String sensorId;
+    private String status;
+    private Double co2Impact;
+
+    private LocalDateTime timestamp;
+
+}
