90b57549e5ffc042a19512266a3148d9eed18072
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-12-09 09:55:36 +0100

    Feat : add JwtUtils for JWT token generation and validation

---
+++ src/main/java/com/greentechinnovators/backend/security/JwtUtils.java
@@ -0,0 +1,62 @@
+package com.greentechinnovators.backend.security;
+
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.List;
+
+@Component
+public class JwtUtils {
+
+    @Value("${app.jwtSecret}")
+    private String secretKey;
+
+    @Value("${app.jwtExpirationMs}")
+    private int jwtExpirationMs;
+
+    public  String generateToken(Authentication authentication) {
+        UserDetails userPrincipal = (UserDetails) authentication.getPrincipal();
+        List<String> roles = userPrincipal.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+
+        return JWT.create()
+                .withSubject(userPrincipal.getUsername())
+                .withClaim("roles" , roles)
+                .withIssuedAt(Instant.now())
+                .withExpiresAt(Instant.now().plusSeconds(jwtExpirationMs))
+                .sign(Algorithm.HMAC256(secretKey));
+    }
+
+
+    public Boolean validateToken(String token) {
+        try {
+            JWT.require(Algorithm.HMAC256(secretKey))
+                    .build()
+                    .verify(token);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public String extractUsername(String token) {
+        return JWT.decode(token).getSubject();
+    }
+
+    public List<String> extractRoles(String token) {
+        return JWT.decode(token).getClaim("roles").asList(String.class);
+    }
+
+
+
+
+}
