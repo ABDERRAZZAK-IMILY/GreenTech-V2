121c476c3399a8d422af5b86e06551c8365b0495
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-12-10 14:44:19 +0100

    Removed one import statement from TransportMapper.java

--- GreenTech-V2/src/main/java/com/greentechinnovators/backend/mapper/TransportMapper.java
+++ GreenTech-V2/src/main/java/com/greentechinnovators/backend/mapper/TransportMapper.java
@@ -1,6 +1,5 @@
 package com.greentechinnovators.backend.mapper;
 
-import com.greentechinnovators.backend.dto.TransportDataDto;
 import com.greentechinnovators.backend.entity.TransportData;
 import org.springframework.stereotype.Component;
 
