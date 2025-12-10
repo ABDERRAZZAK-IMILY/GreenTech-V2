f96419031282bb28a570a3d2c86c18d92c7bd39c
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-12-10 12:09:19 +0100

    Feat: add TrashRepository for managing Trash entities in MongoDB

---
+++ GreenTech-V2/src/main/java/com/greentechinnovators/backend/repository/TrashRepository.java
@@ -0,0 +1,12 @@
+package com.greentechinnovators.backend.repository;
+
+import com.greentechinnovators.backend.entity.Trash;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TrashRepository extends MongoRepository<Trash, String> {
+    List<Trash> findAllByOrderByTimestampDesc();
+}
