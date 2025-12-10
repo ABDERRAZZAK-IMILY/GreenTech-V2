63ddb20de9e7b1ec3ad67505d5754e44716a3ce2
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-12-10 12:08:33 +0100

    Feat: enhance Trash entity with Lombok annotations for cleaner code

--- GreenTech-V2/src/main/java/com/greentechinnovators/backend/entity/Trash.java
+++ GreenTech-V2/src/main/java/com/greentechinnovators/backend/entity/Trash.java
@@ -1,9 +1,6 @@
 package com.greentechinnovators.backend.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -14,6 +11,8 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Document("Trash")
+@Data
+@Builder
 public class Trash {
     @Id
     private String id;
