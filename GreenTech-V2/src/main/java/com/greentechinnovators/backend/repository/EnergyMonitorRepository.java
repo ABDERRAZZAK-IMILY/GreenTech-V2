010809a3dfb7da73049afe907d9134d6060f72eb
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-12-10 15:30:36 +0100

    Add VehicleMapper class and implement mapping methods for Vehicle and VehicleLog entities; add findByMacAddress method to EnergyMonitor and TrashMonitor repositories

--- GreenTech-V2/src/main/java/com/greentechinnovators/backend/repository/EnergyMonitorRepository.java
+++ GreenTech-V2/src/main/java/com/greentechinnovators/backend/repository/EnergyMonitorRepository.java
@@ -3,5 +3,8 @@ package com.greentechinnovators.backend.repository;
 import com.greentechinnovators.backend.entity.EnergyMonitor;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface EnergyMonitorRepository extends MongoRepository<EnergyMonitor, String> {
+    Optional<EnergyMonitor> findByMacAddress(String macAddress);
 }
--- GreenTech-V2/src/main/java/com/greentechinnovators/backend/repository/TrashMonitorRepository.java
+++ GreenTech-V2/src/main/java/com/greentechinnovators/backend/repository/TrashMonitorRepository.java
@@ -3,5 +3,8 @@ package com.greentechinnovators.backend.repository;
 import com.greentechinnovators.backend.entity.TrashMonitor;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface TrashMonitorRepository extends MongoRepository<TrashMonitor, String> {
+    Optional<TrashMonitor> findByMacAddress(String macAddress);
 }
