6b8300fc2c3f823c887310d753c0baacfe1b9ee9
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-12-11 10:28:39 +0100

    added update method in theenergy monitor controller

--- GreenTech-V2/src/main/java/com/greentechinnovators/backend/controller/EnergyMonitorController.java
+++ GreenTech-V2/src/main/java/com/greentechinnovators/backend/controller/EnergyMonitorController.java
@@ -2,15 +2,13 @@ package com.greentechinnovators.backend.controller;
 
 import com.greentechinnovators.backend.dto.Energy.Request.EnergyMonitorRequestDTO;
 import com.greentechinnovators.backend.dto.Energy.Responce.EnergyMonitorResponseDTO;
+import com.greentechinnovators.backend.dto.StatusChangeDTO;
 import com.greentechinnovators.backend.entity.EnergyMonitor;
 import com.greentechinnovators.backend.service.EnergyMonitorService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
@@ -19,7 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class EnergyMonitorController {
     private final EnergyMonitorService service;
 
-    public ResponseEntity<EnergyMonitorResponseDTO> getEnergyMonitor(@Valid @RequestBody EnergyMonitorRequestDTO dto) {
+    @PostMapping("/create")
+    public ResponseEntity<EnergyMonitorResponseDTO> create(@Valid @RequestBody EnergyMonitorRequestDTO dto) {
         return ResponseEntity.ok(service.create(dto));
+    }
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<EnergyMonitorResponseDTO>  update(@Valid @RequestBody StatusChangeDTO dto, @PathVariable String id) {
+        return ResponseEntity.ok(service.Update(dto, id));
     }
 }
