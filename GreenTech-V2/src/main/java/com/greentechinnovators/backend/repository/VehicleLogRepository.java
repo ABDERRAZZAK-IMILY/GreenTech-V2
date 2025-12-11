cf75e7bfd5d50783d8f70cd8ed3950874676ddbf
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-12-11 16:11:55 +0100

    added findByCreatedAtBetween in VehicleLogRepository

--- GreenTech-V2/src/main/java/com/greentechinnovators/backend/repository/VehicleLogRepository.java
+++ GreenTech-V2/src/main/java/com/greentechinnovators/backend/repository/VehicleLogRepository.java
@@ -4,6 +4,10 @@ import com.greentechinnovators.backend.entity.VehicleLog;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface VehicleLogRepository extends MongoRepository<VehicleLog,String> {
+    List<VehicleLog> findByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
 }
