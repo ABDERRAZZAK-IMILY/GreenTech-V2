c554ea027a2954647a1a2713a525e07cb7fedadd
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-12-11 15:25:15 +0100

    added a method to make service to calculate carbon foot print

--- GreenTech-V2/src/main/java/com/greentechinnovators/backend/repository/EnergyRepository.java
+++ GreenTech-V2/src/main/java/com/greentechinnovators/backend/repository/EnergyRepository.java
@@ -4,9 +4,10 @@ import com.greentechinnovators.backend.entity.Energy;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface EnergyRepository extends MongoRepository<Energy, String> {
-//    List<Energy> findAllByOrderByTimestampDesc();
+    List<Energy> findByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
 }
