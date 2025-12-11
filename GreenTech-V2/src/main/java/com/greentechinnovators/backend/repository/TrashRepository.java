84d2fe721bd60baaa6f802e161603041ed571ff7
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-12-11 16:01:41 +0100

    added new method to get the carbon print between rage of time

--- GreenTech-V2/src/main/java/com/greentechinnovators/backend/repository/TrashRepository.java
+++ GreenTech-V2/src/main/java/com/greentechinnovators/backend/repository/TrashRepository.java
@@ -4,9 +4,11 @@ import com.greentechinnovators.backend.entity.Trash;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface TrashRepository extends MongoRepository<Trash, String> {
-//    List<Trash> findAllByOrderByTimestampDesc();
+    List<Trash> findByTrashDateBetween(LocalDateTime start, LocalDateTime end);
+
 }
