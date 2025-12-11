00031d7281d48c974f1e1e7179f1178773d10f26
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-12-11 11:48:40 +0100

    feat: complated web soket energy handler

--- GreenTech-V2/src/main/java/com/greentechinnovators/backend/config/WebSocketConfig.java
+++ GreenTech-V2/src/main/java/com/greentechinnovators/backend/config/WebSocketConfig.java
@@ -2,6 +2,8 @@ package com.greentechinnovators.backend.config;
 
 import com.greentechinnovators.backend.handler.Esp32WebSocketHandler;
 import com.greentechinnovators.backend.handler.NotificationWebSocketHandler;
+import com.greentechinnovators.backend.handler.WSEnergyHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -9,20 +11,19 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 
 @Configuration
 @EnableWebSocket
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketConfigurer {
 
     private final Esp32WebSocketHandler esp32Handler;
     private final NotificationWebSocketHandler notificationHandler;
+    private final WSEnergyHandler  wsEnergyHandler;
 
-    public WebSocketConfig(Esp32WebSocketHandler esp32Handler, 
-                          NotificationWebSocketHandler notificationHandler) {
-        this.esp32Handler = esp32Handler;
-        this.notificationHandler = notificationHandler;
-    }
+
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(esp32Handler, "/ws").setAllowedOrigins("*");
         registry.addHandler(notificationHandler, "/notifications").setAllowedOrigins("*");
+        registry.addHandler(wsEnergyHandler, "/wsEnergy").setAllowedOrigins("*");
     }
 }
