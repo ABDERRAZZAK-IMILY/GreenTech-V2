368b6845e28731d7e5dfb0a3dc7867cfe79e34c8
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-12-12 10:28:23 +0100

    made a new dto to get the daily crboon foot print of trash

---
+++ GreenTech-V2/src/main/java/com/greentechinnovators/backend/dto/trash/response/DailyTrashDTO.java
@@ -0,0 +1,9 @@
+package com.greentechinnovators.backend.dto.trash.response;
+
+import java.time.LocalDate;
+
+public class DailyTrashDTO {
+    private LocalDate date;
+    private Double totalWeightKg;
+    private Double carbonFootprintKg;
+}
