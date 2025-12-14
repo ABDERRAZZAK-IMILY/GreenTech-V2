25ad0198c226c89b4c456e0845f4b11e530483f6
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-12-14 17:37:36 +0100

    feat: add OLED detection and configuration functionality with EEPROM support

--- GreenTech-V2/iotWeightSensor/include/oledDetector.h
+++ GreenTech-V2/iotWeightSensor/include/oledDetector.h
@@ -3,8 +3,8 @@
 
 #include <Adafruit_SSD1306.h>
 
-// Initialiser l'OLED avec auto-détection
-// Retourne true si succès, false si échec
+// Initialiser l'OLED avec auto-detection
+// Retourne true si succes, false si echec
 bool initOLED(Adafruit_SSD1306 &display);
 
 #endif
