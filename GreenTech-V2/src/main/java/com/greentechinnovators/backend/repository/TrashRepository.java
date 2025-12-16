1c3079ae2b04dd62caa2c4b1b5ee92778785e203
MohamedMoustir <itsmoustir@gmail.com>
2025-12-16 21:52:52 +0100

    refactor: replace SmsService with EmailService and update alert configuration

--- GreenTech-V2/src/main/java/com/greentechinnovators/backend/repository/TrashRepository.java
+++ GreenTech-V2/src/main/java/com/greentechinnovators/backend/repository/TrashRepository.java
@@ -14,27 +14,6 @@ public interface TrashRepository extends MongoRepository<Trash, String> {
     List<Trash> findAllByOrderByCreatedAtDesc();
 
 
-    @Aggregation(pipeline = {
-            "{ '$match': { 'createdAt': { '$gte': ?0 } } }",
-            "{ '$project': { " +
-                    "    'date': { '$dateToString': { 'format': '%Y-%m-%d', 'date': '$createdAt' } }, " +
-                    "    'wight': 1 " +
-                    "} }",
-
-            "{ '$group': { " +
-                    "    '_id': '$date', " +
-                    "    'total': { '$sum': '$wight' } " +
-                    "} }",
-
-            "{ '$project': { " +
-                    "    '_id': 0, " +
-                    "    'date': '$_id', " +
-                    "    'total': 1 " +
-                    "} }",
-
-            "{ '$sort': { 'date': 1 } }"
-    })
-    List<DailyStatProjection> getLast7DaysStats(LocalDateTime startDate);
 
 
 }
--- GreenTech-V2/src/main/java/com/greentechinnovators/backend/repository/VehicleLogRepository.java
+++ GreenTech-V2/src/main/java/com/greentechinnovators/backend/repository/VehicleLogRepository.java
@@ -13,19 +13,5 @@ import java.util.List;
 public interface VehicleLogRepository extends MongoRepository<VehicleLog,String> {
 
 
-    @Aggregation(pipeline = {
-            "{ '$match': { 'createdAt': { '$gte': ?0 } } }",
-            "{ '$group': { '_id': { $dateToString: { format: '%Y-%m-%d', date: '$createdAt' } }, 'total': { '$sum': '$distanceTravelled' } } }",
-
-            "{ '$project': { " +
-                    "    '_id': 0, " +
-                    "    'date': '$_id', " +
-                    "    'total': 1 " +
-                    "} }",
-
-            "{ '$sort': { 'date': 1 } }"
-    })
-    List<DailyStatProjection> getLast7DaysStats(LocalDateTime startDate);
-
 
 }
